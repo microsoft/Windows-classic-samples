79c8a305488e3c00ccb6cd931ac5e59dd2ad5545
Chris Guzak (WINDOWS) <chrisg@microsoft.com>
2016-08-17 06:36:04 +0000

    PR 52427: Merge duplicate XmlLiteReadWithEncoding samples
     - move the goodness from the win7 version to the newer version, then delete the Win7 version
     - remove the link to the desktop toast repo on GitHub as we are going to delete that soon.

--- Samples/XmlLiteReadWithEncoding/cpp/XmlLiteReadWithEncoding.cpp
+++ Samples/XmlLiteReadWithEncoding/cpp/XmlLiteReadWithEncoding.cpp
@@ -13,174 +13,15 @@
 // PARTICULAR PURPOSE.
 //-----------------------------------------------------------------------
 
-#include <ole2.h>
+#include <atlbase.h>
 #include <xmllite.h>
-#include <stdio.h>
 
-#pragma warning(disable : 4127)  // conditional expression is constant
-#define CHKHR(stmt)             do { hr = (stmt); if (FAILED(hr)) goto CleanUp; } while(0)
-#define HR(stmt)                do { hr = (stmt); goto CleanUp; } while(0)
-#define SAFE_RELEASE(I)         do { if (I){ I->Release(); } I = NULL; } while(0)
-
-//implement filestream that derives from IStream
-class FileStream : public IStream
+HRESULT WriteAttributes(IXmlReader* xmlReader)
 {
-    FileStream(HANDLE hFile) 
-    { 
-        _refcount = 1; 
-        _hFile = hFile;
-    }
-
-    ~FileStream() 
-    { 
-        if (_hFile != INVALID_HANDLE_VALUE)
-        {
-            ::CloseHandle(_hFile);
-        }
-    }
-
-public:
-    HRESULT static OpenFile(LPCWSTR pName, IStream ** ppStream, bool fWrite)
-    {
-        HANDLE hFile = ::CreateFileW(pName, fWrite ? GENERIC_WRITE : GENERIC_READ, FILE_SHARE_READ,
-            NULL, fWrite ? CREATE_ALWAYS : OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-
-        if (hFile == INVALID_HANDLE_VALUE)
-            return HRESULT_FROM_WIN32(GetLastError());
-        
-        *ppStream = new FileStream(hFile);
-        
-        if(*ppStream == NULL)
-            CloseHandle(hFile);
-            
-        return S_OK;
-    }
-
-    virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID iid, __RPC__deref_out _Result_nullonfailure_ void __RPC_FAR *__RPC_FAR *ppvObject)
-    {
-        if (!ppvObject)
-            return E_INVALIDARG;
-        (*ppvObject) = NULL;
-
-        if (iid == __uuidof(IUnknown)
-            || iid == __uuidof(IStream)
-            || iid == __uuidof(ISequentialStream))
-        {
-            *ppvObject = static_cast<IStream*>(this);
-            AddRef();
-            return S_OK;
-        } else
-            return E_NOINTERFACE; 
-    }
-
-    virtual ULONG STDMETHODCALLTYPE AddRef(void) 
-    { 
-        return (ULONG)InterlockedIncrement(&_refcount); 
-    }
-
-    virtual ULONG STDMETHODCALLTYPE Release(void) 
-    {
-        ULONG res = (ULONG) InterlockedDecrement(&_refcount);
-        if (res == 0) 
-            delete this;
-        return res;
-    }
-
-    // ISequentialStream Interface
-public:
-    virtual HRESULT STDMETHODCALLTYPE Read(_Out_writes_bytes_to_(cb, *pcbRead) void* pv, _In_ ULONG cb, _Out_opt_ ULONG* pcbRead)
-    {
-        BOOL rc = ReadFile(_hFile, pv, cb, pcbRead, NULL);
-        return (rc) ? S_OK : HRESULT_FROM_WIN32(GetLastError());
-    }
-
-    virtual HRESULT STDMETHODCALLTYPE Write(_In_reads_bytes_(cb) const void* pv, _In_ ULONG cb, _Out_opt_ ULONG* pcbWritten)
-    {
-        BOOL rc = WriteFile(_hFile, pv, cb, pcbWritten, NULL);
-        return rc ? S_OK : HRESULT_FROM_WIN32(GetLastError());
-    }
-
-    // IStream Interface
-public:
-    virtual HRESULT STDMETHODCALLTYPE SetSize(ULARGE_INTEGER)
-    { 
-        return E_NOTIMPL;   
-    }
-    
-    virtual HRESULT STDMETHODCALLTYPE CopyTo(_In_ IStream*, ULARGE_INTEGER, _Out_opt_ ULARGE_INTEGER*, _Out_opt_ ULARGE_INTEGER*) 
-    { 
-        return E_NOTIMPL;   
-    }
-    
-    virtual HRESULT STDMETHODCALLTYPE Commit(DWORD)                                      
-    { 
-        return E_NOTIMPL;   
-    }
-    
-    virtual HRESULT STDMETHODCALLTYPE Revert(void)                                       
-    { 
-        return E_NOTIMPL;   
-    }
-    
-    virtual HRESULT STDMETHODCALLTYPE LockRegion(ULARGE_INTEGER, ULARGE_INTEGER, DWORD)              
-    { 
-        return E_NOTIMPL;   
-    }
-    
-    virtual HRESULT STDMETHODCALLTYPE UnlockRegion(ULARGE_INTEGER, ULARGE_INTEGER, DWORD)            
-    { 
-        return E_NOTIMPL;   
-    }
-    
-    virtual HRESULT STDMETHODCALLTYPE Clone(__RPC__deref_out_opt IStream **)
-    { 
-        return E_NOTIMPL;   
-    }
-
-    virtual HRESULT STDMETHODCALLTYPE Seek(LARGE_INTEGER liDistanceToMove, DWORD dwOrigin, _Out_opt_ ULARGE_INTEGER* lpNewFilePointer)
-    { 
-        DWORD dwMoveMethod;
-
-        switch(dwOrigin)
-        {
-        case STREAM_SEEK_SET:
-            dwMoveMethod = FILE_BEGIN;
-            break;
-        case STREAM_SEEK_CUR:
-            dwMoveMethod = FILE_CURRENT;
-            break;
-        case STREAM_SEEK_END:
-            dwMoveMethod = FILE_END;
-            break;
-        default:   
-            return STG_E_INVALIDFUNCTION;
-            break;
-        }
-
-        if (SetFilePointerEx(_hFile, liDistanceToMove, (PLARGE_INTEGER) lpNewFilePointer,
-                             dwMoveMethod) == 0)
-            return HRESULT_FROM_WIN32(GetLastError());
-        return S_OK;
-    }
-
-    virtual HRESULT STDMETHODCALLTYPE Stat(__RPC__out STATSTG* pStatstg, DWORD )
-    {
-        if (GetFileSizeEx(_hFile, (PLARGE_INTEGER) &pStatstg->cbSize) == 0)
-            return HRESULT_FROM_WIN32(GetLastError());
-        return S_OK;
-    }
-
-private:
-    HANDLE _hFile;
-    LONG _refcount;
-};
-
-HRESULT WriteAttributes(IXmlReader* pReader)
-{
-    const WCHAR* pwszPrefix;
-    const WCHAR* pwszLocalName;
-    const WCHAR* pwszValue;
-    HRESULT hr = pReader->MoveToFirstAttribute();
+    PCWSTR pwszPrefix;
+    PCWSTR pwszLocalName;
+    PCWSTR pwszValue;
+    HRESULT hr = xmlReader->MoveToFirstAttribute();
 
     if (S_FALSE == hr)
         return hr;
@@ -193,20 +34,20 @@ HRESULT WriteAttributes(IXmlReader* pReader)
     {
         for (;;)
         {
-            if (!pReader->IsDefault())
+            if (!xmlReader->IsDefault())
             {
                 UINT cwchPrefix;
-                if (FAILED(hr = pReader->GetPrefix(&pwszPrefix, &cwchPrefix)))
+                if (FAILED(hr = xmlReader->GetPrefix(&pwszPrefix, &cwchPrefix)))
                 {
                     wprintf(L"Error getting prefix, error is %08.8lx", hr);
                     return hr;
                 }
-                if (FAILED(hr = pReader->GetLocalName(&pwszLocalName, NULL)))
+                if (FAILED(hr = xmlReader->GetLocalName(&pwszLocalName, nullptr)))
                 {
                     wprintf(L"Error getting local name, error is %08.8lx", hr);
                     return hr;
                 }
-                if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
+                if (FAILED(hr = xmlReader->GetValue(&pwszValue, nullptr)))
                 {
                     wprintf(L"Error getting value, error is %08.8lx", hr);
                     return hr;
@@ -217,115 +58,111 @@ HRESULT WriteAttributes(IXmlReader* pReader)
                     wprintf(L"%s=\"%s\" ", pwszLocalName, pwszValue);
             }
 
-            if (S_OK != pReader->MoveToNextAttribute())
+            if (S_OK != xmlReader->MoveToNextAttribute())
                 break;
         }
     }
     return hr;
 }
 
-int __cdecl wmain(int argc, _In_reads_(argc) WCHAR* argv[])
+int wmain(int argc, _In_reads_(argc) WCHAR* argv[])
 {
-    HRESULT hr = S_OK;
-    IStream *pFileStream = NULL;
-    IXmlReader *pReader = NULL;
-    IXmlReaderInput *pReaderInput = NULL;
+    CComPtr<IStream> fileStream;
+    CComPtr<IXmlReader> xmlReader;
+    CComPtr<IXmlReaderInput> xmlReaderInput;
     XmlNodeType nodetype;
-    const WCHAR* pwszPrefix;
-    const WCHAR* pwszLocalName;
-    const WCHAR* pwszValue;
+    PCWSTR pwszPrefix;
+    PCWSTR pwszLocalName;
+    PCWSTR pwszValue;
     UINT cwchPrefix;
 
-    if (argc != 2)
-    {
-        wprintf(L"Usage: XmlLiteReadWithEncoding.exe name-of-input-file\n");
-        return 0;
-    }
+    PCWSTR file = (argc == 2) ? argv[1] : L"encoded.xml";
 
     // Open read-only input stream
-    if (FAILED(hr = FileStream::OpenFile(argv[1], &pFileStream, FALSE)))
+    HRESULT hr = SHCreateStreamOnFileW(file, STGM_READ, &fileStream);
+    if (FAILED(hr))
     {
         wprintf(L"Error creating file reader, error is %08.8lx", hr);
-        HR(hr);
+        return -1;
     }
 
-    if (FAILED(hr = CreateXmlReader(__uuidof(IXmlReader),(void**) &pReader, NULL)))
+    if (FAILED(hr = CreateXmlReader(IID_PPV_ARGS(&xmlReader), nullptr)))
     {
         wprintf(L"Error creating xml reader, error is %08.8lx", hr);
-        HR(hr);
+        return -1;
     }
 
-    if (FAILED(hr = CreateXmlReaderInputWithEncodingName(pFileStream, NULL, L"utf-16", FALSE,
-        L"c:\temp", &pReaderInput)))
+    if (FAILED(hr = CreateXmlReaderInputWithEncodingName(fileStream, nullptr, L"utf-16", FALSE,
+        L"c:\temp", &xmlReaderInput)))
     {
         wprintf(L"Error creating xml reader with encoding code page, error is %08.8lx", hr);
-        HR(hr);
+        return -1;
     }
 
-    if (FAILED(hr = pReader->SetInput(pReaderInput)))
+    if (FAILED(hr = xmlReader->SetInput(xmlReaderInput)))
     {
         wprintf(L"Error setting input for reader, error is %08.8lx", hr);
-        HR(hr);
+        return -1;
     }
 
     for (;;)
     {
-        hr = pReader->Read(&nodetype);
+        hr = xmlReader->Read(&nodetype);
         if (S_FALSE == hr)
             break;
         if (S_OK != hr)
         {
             wprintf(L"\nXmlLite Error: %08.8lx\n", hr);
-            HR(hr);
+            return -1;
         }
         switch (nodetype)
         {
         case XmlNodeType_XmlDeclaration:
             wprintf(L"<?xml ");
-            if (FAILED(hr = WriteAttributes(pReader)))
+            if (FAILED(hr = WriteAttributes(xmlReader)))
             {
                 wprintf(L"Error, Method: WriteAttributes, error is %08.8lx", hr);
-                HR(hr);
+                return -1;
             }
             wprintf(L"?>\r\n");
             break;
         case XmlNodeType_Element:
-            if (FAILED(hr = pReader->GetPrefix(&pwszPrefix, &cwchPrefix)))
+            if (FAILED(hr = xmlReader->GetPrefix(&pwszPrefix, &cwchPrefix)))
             {
                 wprintf(L"Error, Method: GetPrefix, error is %08.8lx", hr);
-                HR(hr);
+                return -1;
             }
-            if (FAILED(hr = pReader->GetLocalName(&pwszLocalName, NULL)))
+            if (FAILED(hr = xmlReader->GetLocalName(&pwszLocalName, nullptr)))
             {
                 wprintf(L"Error, Method: GetLocalName, error is %08.8lx", hr);
-                HR(hr);
+                return -1;
             }
             if (cwchPrefix > 0)
                 wprintf(L"<%s:%s ", pwszPrefix, pwszLocalName);
             else
                 wprintf(L"<%s ", pwszLocalName);
 
-            if (FAILED(hr = WriteAttributes(pReader)))
+            if (FAILED(hr = WriteAttributes(xmlReader)))
             {
                 wprintf(L"Error, Method: WriteAttributes, error is %08.8lx", hr);
-                HR(hr);
+                return -1;
             }
 
-            if (pReader->IsEmptyElement() )
+            if (xmlReader->IsEmptyElement())
                 wprintf(L"/>");
             else
                 wprintf(L">");
             break;
         case XmlNodeType_EndElement:
-            if (FAILED(hr = pReader->GetPrefix(&pwszPrefix, &cwchPrefix)))
+            if (FAILED(hr = xmlReader->GetPrefix(&pwszPrefix, &cwchPrefix)))
             {
                 wprintf(L"Error, Method: GetPrefix, error is %08.8lx", hr);
-                HR(hr);
+                return -1;
             }
-            if (FAILED(hr = pReader->GetLocalName(&pwszLocalName, NULL)))
+            if (FAILED(hr = xmlReader->GetLocalName(&pwszLocalName, nullptr)))
             {
                 wprintf(L"Error, Method: GetLocalName, error is %08.8lx", hr);
-                HR(hr);
+                return -1;
             }
             if (cwchPrefix > 0)
                 wprintf(L"</%s:%s>", pwszPrefix, pwszLocalName);
@@ -334,34 +171,34 @@ int __cdecl wmain(int argc, _In_reads_(argc) WCHAR* argv[])
             break;
         case XmlNodeType_Text:
         case XmlNodeType_Whitespace:
-            if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
+            if (FAILED(hr = xmlReader->GetValue(&pwszValue, nullptr)))
             {
                 wprintf(L"Error, Method: GetValue, error is %08.8lx", hr);
-                HR(hr);
+                return -1;
             }
             wprintf(L"%s", pwszValue);
             break;
         case XmlNodeType_CDATA:
-            if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
+            if (FAILED(hr = xmlReader->GetValue(&pwszValue, nullptr)))
             {
                 wprintf(L"Error, Method: GetValue, error is %08.8lx", hr);
-                HR(hr);
+                return -1;
             }
             wprintf(L"<![CDATA[%s]]>", pwszValue);
             break;
         case XmlNodeType_ProcessingInstruction:
-            if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
+            if (FAILED(hr = xmlReader->GetValue(&pwszValue, nullptr)))
             {
                 wprintf(L"Error, Method: GetValue, error is %08.8lx", hr);
-                HR(hr);
+                return -1;
             }
             wprintf(L"<?%s?>", pwszValue);
             break;
         case XmlNodeType_Comment:
-            if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
+            if (FAILED(hr = xmlReader->GetValue(&pwszValue, nullptr)))
             {
                 wprintf(L"Error, Method: GetValue, error is %08.8lx", hr);
-                HR(hr);
+                return -1;
             }
             wprintf(L"<!--%s-->", pwszValue);
             break;
@@ -371,10 +208,5 @@ int __cdecl wmain(int argc, _In_reads_(argc) WCHAR* argv[])
         }
     }
     wprintf(L"\n");
-
-CleanUp:
-    SAFE_RELEASE(pReaderInput);
-    SAFE_RELEASE(pFileStream);
-    SAFE_RELEASE(pReader);
-    return hr;
+    return 0;
 }
