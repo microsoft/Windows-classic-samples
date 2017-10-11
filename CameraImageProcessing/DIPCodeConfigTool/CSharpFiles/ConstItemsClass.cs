e0883a6af1403fbd878e106d5ad9e75facd67817
jimmy hu <60071jimmy@gmail.com>
2017-10-12 02:14:21 +0800

    建立CodeHeader字串物件
    建立CodeHeader字串物件

--- CameraImageProcessing/DIPCodeConfigTool/CSharpFiles/ConstItemsClass.cs
+++ CameraImageProcessing/DIPCodeConfigTool/CSharpFiles/ConstItemsClass.cs
@@ -8,5 +8,12 @@ namespace DIPCodeConfigTool.CSharpFiles                                         
 {																				//	進入命名空間
 	class ConstItemsClass                                                       //	ConstItemsClass類別
 	{                                                                           //	進入ConstItemsClass類別
+		public const string CodeHeader =                                        //	建立CodeHeader字串物件
+			@"/*\tC語言影像處理函式庫 \n \
+				\tDevelop by Jimmy HU<s103360021@gmail.com> \n \ 
+				\tThis program is licensed under GNU General Public License v3. \n \
+				\tDIPDefine.h定義影像處理函數所使用之結構、資料型態 \n \
+				*/";                                                            //	建立CodeHeader內容
+
 	}                                                                           //	結束ConstItemsClass類別
 }																				//	結束命名空間
