a8cdf7f07489f05fcb1d91dadf1a4fdd14455fe9
jimmy hu <60071jimmy@gmail.com>
2017-10-12 01:47:52 +0800

    調整GUI介面配置
    調整GUI介面配置

--- CameraImageProcessing/DIPCodeConfigTool/Form1.Designer.cs
+++ CameraImageProcessing/DIPCodeConfigTool/Form1.Designer.cs
@@ -28,9 +28,17 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.SuspendLayout();
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(769, 462);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
