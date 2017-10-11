1bfad5228decb2c1e8f0b7ac304cca5c1762759b
jimmy hu <60071jimmy@gmail.com>
2017-10-12 00:44:09 +0800

    調整include檔案路徑
    調整include檔案路徑

--- CameraImageProcessing/MFCaptureD3D/ImageProcessing/Conversion.cpp
+++ CameraImageProcessing/MFCaptureD3D/ImageProcessing/Conversion.cpp
@@ -5,8 +5,8 @@
 //-------------------------------------------------------------------
 
 //-----include header files, 引入標頭檔-----
-#include "MFCaptureD3D.h"												//	include "MFCaptureD3D.h" header file, 引入標頭檔MFCaptureD3D.h
-#include "BufferLock.h"													//	include "BufferLock.h" header file, 引入標頭檔BufferLock.h
+#include "..\\MFCaptureD3D.h"											//	include "MFCaptureD3D.h" header file, 引入標頭檔MFCaptureD3D.h
+#include "..\\BufferLock.h"												//	include "BufferLock.h" header file, 引入標頭檔BufferLock.h
 #include "Conversion.h"
 #include <stdio.h>														//	include "stdio.h" header file, 引入標頭檔stdio.h
 
