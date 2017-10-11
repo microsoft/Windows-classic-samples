d0215a6f4ca4bf39a779864c9deb869ef926a1c6
jimmy hu <60071jimmy@gmail.com>
2017-10-12 01:54:58 +0800

    新增註解
    新增註解

--- CameraImageProcessing/DIPCodeConfigTool/CSharpFiles/ConstItemsClass.cs
+++ CameraImageProcessing/DIPCodeConfigTool/CSharpFiles/ConstItemsClass.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using System;                                                                   //	使用System函式庫
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DIPCodeConfigTool.CSharpFiles
-{
-	class ConstItemsClass
-	{
-	}
-}
+namespace DIPCodeConfigTool.CSharpFiles                                         //	DIPCodeConfigTool.CSharpFiles命名空間
+{																				//	進入命名空間
+	class ConstItemsClass                                                       //	ConstItemsClass類別
+	{                                                                           //	進入ConstItemsClass類別
+	}                                                                           //	結束ConstItemsClass類別
+}																				//	結束命名空間
