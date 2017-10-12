a58911755b82a72e22028d7df78e6c011e94d01b
jimmy hu <60071jimmy@gmail.com>
2017-10-12 16:18:01 +0800

    移除重複include檔案
    移除重複include檔案

--- CameraImageProcessing/MFCaptureD3D/ImageProcessing/Conversion.cpp
+++ CameraImageProcessing/MFCaptureD3D/ImageProcessing/Conversion.cpp
@@ -6,7 +6,6 @@
 
 //-----include header files, 引入標頭檔-----
 #include "..\\MFCaptureD3D.h"											//	include "MFCaptureD3D.h" header file, 引入標頭檔MFCaptureD3D.h
-#include "..\\BufferLock.h"												//	include "BufferLock.h" header file, 引入標頭檔BufferLock.h
 #include "Conversion.h"
 
 //-----funtion implementation, 副程式實作-----
