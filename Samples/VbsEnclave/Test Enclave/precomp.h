74df850b6e1f71a339257966214ee110a3816242
Akash Trehan <aktrehan@microsoft.com>
2024-05-17 16:56:00 -0700

    Do not ship debuggable enclaves to production comment

--- Samples/VbsEnclave/Test Enclave/precomp.h
+++ Samples/VbsEnclave/Test Enclave/precomp.h
@@ -26,16 +26,16 @@ Author:
 const IMAGE_ENCLAVE_CONFIG __enclave_config = {
     sizeof(IMAGE_ENCLAVE_CONFIG),
     IMAGE_ENCLAVE_MINIMUM_CONFIG_SIZE,
-    IMAGE_ENCLAVE_POLICY_DEBUGGABLE,
+    IMAGE_ENCLAVE_POLICY_DEBUGGABLE,    // DO NOT SHIP DEBUGGABLE ENCLAVES TO PRODUCTION
     0,
     0,
     0,
-    { 0xFE, 0xFE },     // family id
-    { 0x01, 0x01 },     // image id
-    0,                  // version
-    0,                  // SVN
-    0x10000000,         // size
-    16,                 // number of threads
+    { 0xFE, 0xFE },    // family id
+    { 0x01, 0x01 },    // image id
+    0,                 // version
+    0,                 // SVN
+    0x10000000,        // size
+    16,                // number of threads
     IMAGE_ENCLAVE_FLAG_PRIMARY_IMAGE
 };
 
