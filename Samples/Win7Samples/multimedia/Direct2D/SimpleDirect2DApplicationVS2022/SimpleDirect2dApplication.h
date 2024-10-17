130a59e99e652af61018212a01a05cf00f5eb2aa
Daniel Ozyurt <d.ozyurt@outlook.com>
2024-10-17 23:14:54 +0200

    Target Windows 10 and above

--- Samples/Win7Samples/multimedia/Direct2D/SimpleDirect2DApplicationVS2022/SimpleDirect2dApplication.h
+++ Samples/Win7Samples/multimedia/Direct2D/SimpleDirect2DApplicationVS2022/SimpleDirect2dApplication.h
@@ -11,11 +11,11 @@
 // Modify the following defines if you have to target a platform prior to the ones specified below.
 // Refer to MSDN for the latest info on corresponding values for different platforms.
 #ifndef WINVER              // Allow use of features specific to Windows 7 or later.
-#define WINVER 0x0700       // Change this to the appropriate value to target other versions of Windows.
+#define WINVER NTDDI_WIN10 // Change this to the appropriate value to target other versions of Windows.
 #endif
 
 #ifndef _WIN32_WINNT        // Allow use of features specific to Windows 7 or later.
-#define _WIN32_WINNT 0x0700 // Change this to the appropriate value to target other versions of Windows.
+#define _WIN32_WINNT _WIN32_WINNT_WIN10 // Change this to the appropriate value to target other versions of Windows.
 #endif
 
 #ifndef UNICODE
